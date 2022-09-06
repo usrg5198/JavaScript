c207061e9c49203796cc9239f32d9113fa57efee
iamdevelopstrainer <usrg5198@gmail.com>
2022-09-06 13:34:24 -0400

    Initial commit into a new repository

---
+++ inputprogram.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var fname = prompt ("Enter Your First Name: ");
+var lname = prompt ("Enter Your Last Name: ");
+
+console.log("Your Full Name is",fname,lname);
